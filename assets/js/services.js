0221a0ab3cf9d25fcb70d082e59060f8a414843d
Matt Lea <mlea@schematical.com>
2014-03-16 09:16:17 -0500

    configing

--- assets/js/services.js
+++ assets/js/services.js
@@ -1,7 +1,7 @@
 var nproxy_services = angular.module('nproxy.services', []);
 
 nproxy_services.factory('Apps',  function(){
-    var socket = window.io.connect('http://localhost');
+    var socket = window.io.connect('http://' + document.location.hostname);
     socket.deploy = function(app_id){
         socket.emit('deploy', { app_id:app_id });
     }
